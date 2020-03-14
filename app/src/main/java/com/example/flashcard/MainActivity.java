1f23ec0d0267b2d145065e68c09bfe879c150e28
18xulisa <lnxu@uci.edu>
2020-03-14 11:36:05 -0700

    lab 3

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -9,26 +9,47 @@ import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+
+    int currentCardDisplayedIndex = 0; //keep track of which card is currently shown
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flaschard_question).setOnClickListener(new View.OnClickListener() {
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        // add default card to database
+        String question = ((TextView) findViewById(R.id.flashcard_question)).getText().toString();
+        String answer = ((TextView) findViewById(R.id.flashcard_answer)).getText().toString();
+        flashcardDatabase.insertCard(new Flashcard(question, answer));
+
+        if (allFlashcards != null && allFlashcards.size() > 0){
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+
+        }
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                findViewById(R.id.flaschard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flaschard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
 
-        findViewById(R.id.flaschard_answer).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                findViewById(R.id.flaschard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flaschard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
 
@@ -44,11 +65,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                intent.putExtra("question", ((TextView)findViewById(R.id.flaschard_question)).getText().toString());
-                intent.putExtra("answer", ((TextView)findViewById(R.id.flaschard_answer)).getText().toString());
-                MainActivity.this.startActivityForResult(intent, 100);
+                intent.putExtra("question", ((TextView)findViewById(R.id.flashcard_question)).getText().toString());
+                intent.putExtra("answer", ((TextView)findViewById(R.id.flashcard_answer)).getText().toString());
+                MainActivity.this.startActivityForResult(intent, 200); // 200 is EDIT CARD REQUEST CODE
             }
         });
+
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // advance pointer index to show next card
+                currentCardDisplayedIndex++;
+
+                // make sure IndexOutOfBoundsError doesn't occur if viewing last card
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set question and answer TextViews with data from database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+
     }
 
     @Override
@@ -56,14 +98,23 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult
             String question = data.getExtras().getString("question"); // 'question' matches key used when string put in intent
             String answer = data.getExtras().getString("answer");
+            findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+
+
+            // store card in database
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+
+            //local variable that holds list of flashcards is updated
+            allFlashcards = flashcardDatabase.getAllCards();
 
             // set the text for the flashcard question and answer
-            ((TextView) findViewById(R.id.flaschard_question)).setText(question);
-            ((TextView) findViewById(R.id.flaschard_answer)).setText(answer);
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
 
 
             // display snackbar message
-            Snackbar.make(findViewById(R.id.flaschard_question), "Card successfully created!", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully created!", Snackbar.LENGTH_SHORT).show();
 
         }
 
